f86bf0e8990f5c6c016a52443c8feea15aeaf693
jasonpizzaboy <80494361+taekyunJason@users.noreply.github.com>
2022-04-12 11:14:56 +0900

    package-lock.json 오류 수정

---
+++ app 2.js
@@ -0,0 +1,39 @@
+//express 모듈 불러오기
+const express = require("express");
+const mongoose = require("mongoose");
+const connect = require("./schemas/index");
+const app = express();
+const port = 3000;
+
+connect();
+
+//접속 로그
+const requestMiddleware = (req, res, next) => {
+  console.log("request Url : ", req.originalUrl, "-", new Date());
+  next();
+};
+
+//라우터 불러오기
+const loginRouter = require("./routes/logins");
+
+//미들웨어
+app.use(express.json());
+app.use(requestMiddleware);
+
+//라우터 연결
+app.use("/", [loginRouter]);
+
+app.get("/", (req, res) => {
+  console.log("로그인 화면 입니다.");
+  res.sendFile(__dirname + "/templates/index.html");
+});
+
+app.get("/main", (req, res) => {
+  console.log("로그인 화면 입니다.");
+  res.sendFile(__dirname + "/templates/main.html");
+});
+
+//서버 연결
+app.listen(port, () => {
+  console.log(port, "번으로 서버가 켜졌어요!");
+});
