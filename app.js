d08686bbd7c44f4b6ab14d48f7f0d7996d12b49f
Lee MiHwa <bbs2824@naver.com>
2022-04-12 21:08:10 +0900

    multer 추가중

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ const Http = require("http");
 const app = express();
 const http = Http.createServer(app);
 
+const multer = require('multer'); // express에 multer모듈 적용 (for 파일업로드)
+const upload = multer({ dest: 'uploads/' })
+// 입력한 파일이 uploads/ 폴더 내에 저장된다.
+// multer라는 모듈이 함수라서 함수에 옵션을 줘서 실행을 시키면, 해당 함수는 미들웨어를 리턴한다.
 
 app.get('/', (req, res) => {
 	res.render("../views/postingForm");
+
+});
+
+// 업로드 - 파일 업로드 폼
+app.get('/upload', function(req, res){
+  res.render('upload');
+});
+
+app.post('/upload', function(req, res){
+  res.send('업로드 성공!');
+});
+
+app.post('/upload', upload.single('userfile'), function(req, res){
+  res.send('Uploaded! :'+req.file); // object를 리턴함
+  console.log(req.file); // 콘솔(터미널)을 통해서 req.file Object 내용 확인 가능.
 });
 
 const router = require("./routes/posts");
